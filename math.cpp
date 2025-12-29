e48163a59f5cbbd3b9cef96b5f6f6644cf2238a7
nrynmish <narayan16sept@gmail.com>
2025-12-30 05:14:44 +0530

    Learned extraction of digits and use

---
+++ math.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void countDigits(int n) {
+    int count = 0;
+    while (n>0) {
+        n= n/10 ;
+        count++ ;
+    }
+    cout << count << endl;
+}
+
+void reverseNumber(int n) {
+    
+    while (n>0) {
+        int digit = n % 10;
+        cout << digit;
+        n = n / 10;
+    }
+    cout << endl;
+}
+
+void reverseNumber2(int n) {
+    int reversedNum = 0;
+    while (n>0) {
+        int lastDigit = n % 10;
+        reversedNum = reversedNum * 10 + lastDigit;
+        n = n / 10;
+    }
+    cout << reversedNum << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    // countDigits(n);
+    // reverseNumber(n); // just spits out digits and not actually revrresing the number
+    reverseNumber2(n); // actually reverses the number
+    return 0;
+}
