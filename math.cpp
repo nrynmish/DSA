2a250849aff5f5b2b7b5e1a46c09b5f4e0ddf423
nrynmish <narayan16sept@gmail.com>
2025-12-30 17:53:40 +0530

    Learned palindrome and Euclidean Algo

--- math.cpp
+++ math.cpp
@@ -31,11 +31,27 @@ void reverseNumber2(int n) {
     cout << reversedNum << endl;
 }
 
+void checkPalindrome(int n) {
+    int originalNum = n;
+    int reversedNum = 0;
+    while (n>0) {
+        int lastDigit = n % 10;
+        reversedNum = reversedNum * 10 + lastDigit;
+        n = n / 10;
+    }
+    if (originalNum == reversedNum) {
+        cout << "Palindrome" << endl;
+    } else {
+        cout << "Not a Palindrome" << endl;
+    }
+}   
+
 int main() {
     int n;
     cin >> n;
     // countDigits(n);
     // reverseNumber(n); // just spits out digits and not actually revrresing the number
-    reverseNumber2(n); // actually reverses the number
+    // reverseNumber2(n); // actually reverses the number
+    checkPalindrome(n);
     return 0;
 }
