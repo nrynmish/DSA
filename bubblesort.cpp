b32834c054bda905353cf01ce8ebfc2171259971
nrynmish <narayan16sept@gmail.com>
2026-01-18 02:23:52 +0530

    Learned Bubble Sort

---
+++ bubblesort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubblesort(int arr[], int n) {
+    for (int i=n-1; i>=1; i--) {
+
+        for (int j=0; j<=i-1 ; j++)
+        {
+            if (arr[j] > arr[j+1]) {
+                //int temp = arr[j+1];
+                //arr[j+1] = arr[j];
+                //arr[j] =  temp;
+                 swap(arr[j], arr[j+1]);
+                
+            }
+        }
+        
+
+    }
+
+}
+
+int main() {
+    int n;
+    cin >> n; // size of array
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; // input array elements
+    }
+
+    bubblesort(arr, n);
+    cout << "sorted array:" ;
+    for (int i = 0; i<n; i++) {
+        cout << arr[i] << " ";
+    } 
+    cout << endl;
+
+return 0;
+}
