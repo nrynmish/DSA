afd64ec930fbcd49ca908266153d94cfe26a23bf
nrynmish <narayan16sept@gmail.com>
2026-08-09 05:31:18 +0530

    Rotate image

---
+++ arrays/rotateimage.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    rotate(matrix);
+
+    cout << "Rotated matrix:" << endl;
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
